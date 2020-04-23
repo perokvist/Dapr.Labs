81a469070e0d675c8c9552e695ca03034ae9d02f
Per Ökvist <perokvist@users.noreply.github.com>
2020-04-23 18:07:02 +0200

    util endpoint for binding and pubsub

--- CSharpSample/SampleController.cs
+++ CSharpSample/SampleController.cs
@@ -3,11 +3,12 @@ using Dapr.Client;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DaprSamples
 {
-    public class SampleController : ControllerBase
+    public class SampleController : Controller
     {
         private readonly ILogger logger;
         public const string StoreName = "statestore";
@@ -35,11 +36,45 @@ namespace DaprSamples
             return Ok();
         }
 
+        [HttpPost("pub")]
+        public async Task<ActionResult> PubPostAsync([FromServices] DaprClient daprClient)
+        {
+            this.logger.LogInformation("PUB Start");
+
+            foreach (var e in Enumerable.Range(0, 20))
+            {
+                await daprClient.PublishEventAsync("sample", e);
+            }
+            this.logger.LogInformation("PUB End");
+            return Ok();
+        }
+
+        [HttpPost("bind")]
+        public async Task<ActionResult> BindPostAsync([FromServices] DaprClient daprClient)
+        {
+            this.logger.LogInformation("BIND Start");
+
+            foreach (var e in Enumerable.Range(0, 20))
+            {
+                await daprClient.InvokeBindingAsync("hub", e);
+            }
+            this.logger.LogInformation("BIND End");
+            return Ok();
+        }
+
         [Topic("sample")]
         [HttpPost("sample")]
-        public ActionResult Post()
+        public async Task<ActionResult> PostAsync()
         {
             this.logger.LogInformation("C# got event (pub/sub");
+            using (var reader = new StreamReader(Request.Body))
+            {
+                var body = await reader.ReadToEndAsync();
+                this.logger.LogInformation(body);
+            }
+
+            await Task.Delay(200);
+            this.logger.LogInformation("C# done with event (pub/sub");
             return Ok();
         }
 
@@ -52,7 +87,8 @@ namespace DaprSamples
                 var body = await reader.ReadToEndAsync();
                 this.logger.LogInformation(body);
             }
-            return Ok();
+            this.logger.LogInformation("C# done event (binding)");
+            return base.Json(new { Message = "hello" });
         }
 
     }
